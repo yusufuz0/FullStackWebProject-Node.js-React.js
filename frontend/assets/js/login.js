b5c4cc71c530d64d083dd158e04e31c7b0a46620
Yusuf <133169287+yusufuz0@users.noreply.github.com>
2025-04-21 14:00:42 +0300

    Registration and login procedures have been completed. #1

---
+++ frontend/assets/js/login.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const res = await fetch('http://localhost:5000/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+      alert('Login successful!');
+      window.location.href = '../pages/index.html'; // Anasayfaya yönlendiriyoruz
+    } else {
+      alert(data.message || 'Login failed');
+    }
+  } catch (error) {
+    console.error(error);
+    alert('An error occurred');
+  }
+});
